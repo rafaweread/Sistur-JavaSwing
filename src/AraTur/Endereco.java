bd76cbc8e2d3212586000e818a6de7775d4f15b0
rpl92 <rpl92@rpl92-CL341>
2016-03-24 13:23:04 -0400

    Tela de cadastro de usuário pronto

---
+++ src/AraTur/Endereco.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AraTur;
+
+/**
+ *
+ * @author Rafael Lemos
+ */
+public class Endereco {
+    
+    private String rua;
+    private String bairro;
+    private String numero;
+    private String cidade;
+    private String cep;
+    
+}
---
+++ src/AraTur/Pessoa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AraTur;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Rafael Lemos
+ */
+public class Pessoa extends Endereco{
+    
+    private String Nome;
+    private String Rg;
+    private Date   DataNascimento;
+    private char   Sexo;
+    private Endereco endereco;
+ 
+}
